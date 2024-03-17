c4f3db1e766045d8b62ba1f76af80e4cd4613d29
Pazaz <pazaz@itspazaz.com>
2024-03-17 00:57:26 -0400

    fix: Wrong command name...

--- src/lostcity/script/handlers/ServerOps.ts
+++ src/lostcity/script/handlers/ServerOps.ts
@@ -1,6 +1,6 @@
 import ServerScriptCommand from '../ServerScriptCommands.js';
 import ServerScriptState from '../ServerScriptState.js';
 
-ServerScriptCommand.PUSH_CONSTANT_INT.handler = (state: ServerScriptState): void => {
+ServerScriptCommand.MAP_LOBBY.handler = (state: ServerScriptState): void => {
     state.pushInt(ServerScriptState.MAP_LOBBY ? 1 : 0);
 }
